f2cf4e74cd2a51bfd765fd6e66e08e72e4b29460
oosuhyun <tngusimg@naver.com>
2023-08-05 19:33:45 +0900

    Initial commit

---
+++ src/main/java/com/example/LogisticsWarehouseManagement/LogisticsWarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogisticsWarehouseManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsWarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsWarehouseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LogisticsWarehouseManagement/LogisticsWarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogisticsWarehouseManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsWarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

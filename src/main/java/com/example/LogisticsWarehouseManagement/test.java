c2abbd5a19dbbf75c31309dca5e0e11ee4973989
oosuhyun <tngusimg@naver.com>
2023-08-05 19:57:32 +0900

    commit test

---
+++ src/main/java/com/example/LogisticsWarehouseManagement/test.java
@@ -0,0 +1,13 @@
+package com.example.LogisticsWarehouseManagement;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class test {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
